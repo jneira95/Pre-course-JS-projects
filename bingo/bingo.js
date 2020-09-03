4ae41219c5812df73b7c18e8bf8c8e831866d106
Jorge <jorgengarriga@gmail.com>
2020-09-04 01:49:39 +0200

    Parte bingo, intentando crear carton con Numeros

--- bingo/bingo.js
+++ bingo/bingo.js
@@ -1,27 +1,76 @@
+/* BINGO GAME! */
 
 const bingo = () => {
     let userName;
-    // let score = 0;
-    // let turns = 0;
-    // let card = {
-    //     B: [],
-    //     I: [],
-    //     N: [],
-    //     G: [],
-    //     O: [],
-    // }
+    let userPoints = 0;
+    let userTurns = 0;
+    let userCardboard = {
+        B: { RangeNumbers: [1, 20], Value: [] },
+        I: { RangeNumbers: [21, 40], Value: [] },
+        N: { RangeNumbers: [41, 60], Value: [] },
+        G: { RangeNumbers: [61, 80], Value: [] },
+        O: { RangeNumbers: [81, 100], Value: [] },
+    }
 
     const wellcome = () => {
-        // let nameOk = true;
-        // do {
-        //     userName = prompt("Bienvenido a BINGO GAME!, inserte su Nombre para comenzar a jugar")
-        //     if (userName == "" || userName == null) continue;
-        //     else nameOk = false
-        // } while (nameOk);
-        // console.log(userName);
-        console.log("hola");
+        do {
+            userName = prompt("Bienvenido a BINGO GAME! \nInserte su nombre para comenzar a jugar!");
+        } while (isFinite(userName) || userName == null);
+
+        const startGame = confirm(`Bienvenido ${userName}! \nListo para jugar?`);
+        if (startGame === true) menuGame();
+        else console.log("Vuelve pronto!");
     }
+
+    const menuGame = () => {
+        const userOption = parseInt(prompt("1- Nuevo Carton \n2- ¿Como jugar? \n3- Salir"));
+        switch (userOption) {
+            case 1:
+                let z;
+                while (z === true) {
+                    const userOption1 = parseInt(prompt("1- No me gusta, dame uno nuevo!, \n2- Este SI! Vamos a jugar"));
+                    switch (userOption1) {
+                        case 1:
+                            getNewCardBoard();
+                            break;
+                        case 2:
+
+                            break;
+                        default:
+                            continue;
+                    }
+                }
+                break;
+            case 2:
+
+                break;
+            case 3:
+                console.log("Gracias por jugar, vuelve pronto!");
+                break;
+            default:
+                menuGame();
+        }
+    }
+
+    const getNewCardBoard = () => {
+        const arBingoLetters = ["B", "I", "N", "G", "O"]
+        for (let r = 0; r < arBingoLetters.length; r++) {
+            for (let c = 0; c < 5; c++) {
+                let num = randomIntFromInterval(userCardboard[arBingoLetters[r]].RangeNumbers[0], userCardboard[arBingoLetters[r]].RangeNumbers[1])
+                console.log(num);
+            }
+        }
+
+        function randomIntFromInterval(min, max) {
+            return Math.floor(Math.random() * (max - min + 1) + min);
+        }
+
+    }
+
+
+
+    wellcome()
 }
 
 
-bingo();
+// bingo();
