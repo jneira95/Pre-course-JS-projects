4d6436a17b927e2af8aef09f0240da939ba1e235
Jorge <jorgengarriga@gmail.com>
2020-09-03 20:44:50 +0200

    error en bingo

--- bingo/bingo.js
+++ bingo/bingo.js
@@ -1,4 +1,27 @@
-// PRUEBA 2 De git
-//Se ve??
-//recibido
-// ultima pruba
+
+const bingo = () => {
+    let userName;
+    // let score = 0;
+    // let turns = 0;
+    // let card = {
+    //     B: [],
+    //     I: [],
+    //     N: [],
+    //     G: [],
+    //     O: [],
+    // }
+
+    const wellcome = () => {
+        // let nameOk = true;
+        // do {
+        //     userName = prompt("Bienvenido a BINGO GAME!, inserte su Nombre para comenzar a jugar")
+        //     if (userName == "" || userName == null) continue;
+        //     else nameOk = false
+        // } while (nameOk);
+        // console.log(userName);
+        console.log("hola");
+    }
+}
+
+
+bingo();
