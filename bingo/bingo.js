0548173e0c2ececf1a0d14ec5356e677701d1211
Jorge <jorgengarriga@gmail.com>
2020-09-05 19:22:33 +0200

    update, bingo 90 numeros conseguido, con sus casillas en blanco, toca seguir con las bolas aleatorias

--- bingo/bingo.js
+++ bingo/bingo.js
@@ -5,14 +5,18 @@ const bingo = () => {
     let userPlaying = false;
     let userTurns = 0;
     let line = "No"
-    let newDisplay = [[], [], [], [], []]
+    let newDisplay = [[], [], []];
     let ballDispenserNumbers = [];
     let cardBoard = [
-        { RangeOfNumbers: [1, 20], Value: [], MatchLine: false, letter: "B" },
-        { RangeOfNumbers: [21, 40], Value: [], MatchLine: false, letter: "I" },
-        { RangeOfNumbers: [41, 60], Value: [], MatchLine: false, letter: "N" },
-        { RangeOfNumbers: [61, 80], Value: [], MatchLine: false, letter: "G" },
-        { RangeOfNumbers: [81, 100], Value: [], MatchLine: false, letter: "O" },
+        { RangeOfNumbers: [1, 9], Value: [], MatchLine: false},
+        { RangeOfNumbers: [10, 19], Value: [], MatchLine: false},
+        { RangeOfNumbers: [20, 29], Value: [], MatchLine: false },
+        { RangeOfNumbers: [30, 39], Value: [], MatchLine: false },
+        { RangeOfNumbers: [40, 49], Value: [], MatchLine: false },
+        { RangeOfNumbers: [50, 59], Value: [], MatchLine: false },
+        { RangeOfNumbers: [60, 69], Value: [], MatchLine: false },
+        { RangeOfNumbers: [70, 79], Value: [], MatchLine: false },
+        { RangeOfNumbers: [80, 90], Value: [], MatchLine: false },
     ];
 
     const wellcome = () => {
@@ -84,7 +88,7 @@ const bingo = () => {
     }
 
     const clearStoredData = () => {
-        newDisplay = [[], [], [], [], []];
+        newDisplay = [[], [], []];
     }
 
     const cleanCardBoard = () => {
@@ -95,21 +99,31 @@ const bingo = () => {
     }
 
     const generateBallDispenser = () => {
-        for (let x = 0; x < cardBoard.length; x++) {
-            for (let z = cardBoard[x].RangeOfNumbers[0]; z <= cardBoard[x].RangeOfNumbers[1]; z++) {
-                ballDispenserNumbers.push(cardBoard[x].letter + "-" + z);
-            }
+        for (let x = 1; x <= 90 ; x++) {
+            ballDispenserNumbers.push(x)
         }
     }
     
     const generateNewCardboard = () => {
         for (let x = 0; x < cardBoard.length; x++) {
-            while (cardBoard[x].Value.length < 5) {
-                let num = generateRandomNumber(cardBoard[x].RangeOfNumbers[1], cardBoard[x].RangeOfNumbers[0]);
+            while (cardBoard[x].Value.length < 3) {
+                let num = generateRandomNumber(cardBoard[x].RangeOfNumbers[0], cardBoard[x].RangeOfNumbers[1]);
                 if (cardBoard[x].Value.indexOf(num) === -1) cardBoard[x].Value.push(num);
                 else continue;
             }
         }
+        for (let x = 0; x < cardBoard.length; x++) {
+            for (let z = 0; z < cardBoard[x].Value.length; z++) {
+                newDisplay[z].push(cardBoard[x].Value[z]);
+            }
+        }
+        for (let x = 0; x < 3; x++) {
+            for (let z = 0; z < 4; z++) {
+                let randomFillCardBoard = generateRandomNumber(0, 8)
+                newDisplay[x][randomFillCardBoard] = "#";
+            }
+        }
+
     }
     
     const generateRandomNumber = (min, max) => {
@@ -118,15 +132,9 @@ const bingo = () => {
     
     const showCardBoard = () => {
         console.clear()
-        let bingoName = cardBoard.map(letter => letter.letter);
-        console.log(`\t\t|\t${bingoName.join("\t\t")}\t|`);
-        for (let x = 0; x < 5; x++) {
-            for (let z = 0; z < 5; z++) {
-                newDisplay[z].push(cardBoard[x].Value[z]);
-            }
-        }
+
         for (let i = 0; i < newDisplay.length; i++) {
-            console.log(`\t\t|\t${newDisplay[i].join("\t--\t")}\t|`);
+            console.log(`\t\t|\t${newDisplay[i].join("\t\t")}\t|`);
         }
         if (userPlaying === false) secondMenuGame();
     }
@@ -136,10 +144,12 @@ const bingo = () => {
         showCardBoard();
         userTurns = userTurns + 1;
         let randomBall = generateRandomNumber(0, ballDispenserNumbers.length -1);
-        let matchNumber = ballDispenserNumbers[randomBall].replace(/\D/g, '');
-        console.log(typeof(matchNumber));
-        if (ballDispenserNumbers.length > 0) console.log(`\t\t\tBola N.º: ${ballDispenserNumbers[randomBall]} Turnos: ${userTurns} linea: ${line}`);
-        ballDispenserNumbers.splice(randomBall, 1);
+        let matchNumber
+        if (ballDispenserNumbers.length > 0) {
+            matchNumber = ballDispenserNumbers[randomBall].replace(/\D/g, '');
+            console.log(`\t\t\tBola N.º: ${ballDispenserNumbers[randomBall]} Turnos: ${userTurns} linea: ${line}`);
+            ballDispenserNumbers.splice(randomBall, 1);
+        }
         for (let x = 0; x < cardBoard.length; x++) {
             for (let z = 0; z < cardBoard[x].Value.length; z++) {
                 if (cardBoard[x].Value[z] == matchNumber) cardBoard[x].Value[z] = "X";
