0d0b5fb397f93ab14dd7d187fea19e08207a5302
Jorge <jorgengarriga@gmail.com>
2020-09-06 14:49:07 +0200

    Casi acabado

--- bingo/bingo.js
+++ bingo/bingo.js
@@ -4,19 +4,21 @@ const bingo = () => {
     let userName;
     let userPlaying = false;
     let userTurns = 0;
-    let line = "No"
+    let lineMatch = false
+    let bingoMatch = false
     let newDisplay = [[], [], []];
+    let lines = [[], [], []];
     let ballDispenserNumbers = [];
     let cardBoard = [
-        { RangeOfNumbers: [1, 9], Value: [], MatchLine: false},
-        { RangeOfNumbers: [10, 19], Value: [], MatchLine: false},
-        { RangeOfNumbers: [20, 29], Value: [], MatchLine: false },
-        { RangeOfNumbers: [30, 39], Value: [], MatchLine: false },
-        { RangeOfNumbers: [40, 49], Value: [], MatchLine: false },
-        { RangeOfNumbers: [50, 59], Value: [], MatchLine: false },
-        { RangeOfNumbers: [60, 69], Value: [], MatchLine: false },
-        { RangeOfNumbers: [70, 79], Value: [], MatchLine: false },
-        { RangeOfNumbers: [80, 90], Value: [], MatchLine: false },
+        { RangeOfNumbers: [1, 9], Value: [] },
+        { RangeOfNumbers: [10, 19], Value: [] },
+        { RangeOfNumbers: [20, 29], Value: [] },
+        { RangeOfNumbers: [30, 39], Value: [] },
+        { RangeOfNumbers: [40, 49], Value: [] },
+        { RangeOfNumbers: [50, 59], Value: [] },
+        { RangeOfNumbers: [60, 69], Value: [] },
+        { RangeOfNumbers: [70, 79], Value: [] },
+        { RangeOfNumbers: [80, 90], Value: [] },
     ];
 
     const wellcome = () => {
@@ -52,12 +54,22 @@ const bingo = () => {
         const userOption = prompt("1- Generar nuevo carton! \n2- Vamos a jugar! \n3- Atras");
         switch (userOption) {
             case "1":
-                clearStoredData(), cleanCardBoard();
+                clearStoredData();
+                cleanCardBoard();
                 generateNewCardboard();
                 showCardBoard();
                 break;
             case "2":
-                playingMenuGame();
+                if (cardBoard[0].Value.length === 0) {
+                    generateBallDispenser();
+                    generateNewCardboard();
+                    showCardBoard();
+                    playingTheGame();
+                } else {
+                    clearStoredData();
+                    generateBallDispenser()
+                    playingMenuGame();
+                }
                 break;
             case "3":
                 mainMenuGame();
@@ -88,7 +100,13 @@ const bingo = () => {
     }
 
     const clearStoredData = () => {
-        newDisplay = [[], [], []];
+        // userPlaying = false;
+        userTurns = 0;
+        lineMatch = false;
+        bingoMatch = false;
+        ballDispenserNumbers = [];
+        // newDisplay = [[], [], []];
+        lines = [[], [], []];
     }
 
     const cleanCardBoard = () => {
@@ -96,6 +114,7 @@ const bingo = () => {
             cardBoard[x].Value = [];
             cardBoard[x].MatchLine = false;
         }
+        newDisplay = [[], [], []];
     }
 
     const generateBallDispenser = () => {
@@ -117,13 +136,17 @@ const bingo = () => {
                 newDisplay[z].push(cardBoard[x].Value[z]);
             }
         }
+        let n;
         for (let x = 0; x < 3; x++) {
-            for (let z = 0; z < 4; z++) {
+            n = 0;
+            while (n < 4) {
                 let randomFillCardBoard = generateRandomNumber(0, 8)
-                newDisplay[x][randomFillCardBoard] = "#";
+                if (newDisplay[x][randomFillCardBoard] !== `#`) {
+                    newDisplay[x].splice(randomFillCardBoard, 1, `#`)
+                    n++
+                } else continue;
             }
         }
-
     }
     
     const generateRandomNumber = (min, max) => {
@@ -132,32 +155,47 @@ const bingo = () => {
     
     const showCardBoard = () => {
         console.clear()
-
         for (let i = 0; i < newDisplay.length; i++) {
-            console.log(`\t\t|\t${newDisplay[i].join("\t\t")}\t|`);
+            console.log(`\t\t|\t${newDisplay[i].join("\t")}\t|`);
         }
         if (userPlaying === false) secondMenuGame();
     }
     
     const playingTheGame = () => {
-        clearStoredData();
         showCardBoard();
         userTurns = userTurns + 1;
-        let randomBall = generateRandomNumber(0, ballDispenserNumbers.length -1);
-        let matchNumber
+        let randomIndexBall = generateRandomNumber(0, ballDispenserNumbers.length - 1);
+        let getBall = ballDispenserNumbers[randomIndexBall]
+        for (let x = 0; x < newDisplay.length; x++) {
+            for (let z = 0; z < newDisplay[x].length; z++) {
+                if (newDisplay[x][z] == getBall) {
+                    newDisplay[x][z] = "X"
+                    lines[x].push("X")
+                }
+            }
+        }
         if (ballDispenserNumbers.length > 0) {
-            matchNumber = ballDispenserNumbers[randomBall].replace(/\D/g, '');
-            console.log(`\t\t\tBola N.º: ${ballDispenserNumbers[randomBall]} Turnos: ${userTurns} linea: ${line}`);
-            ballDispenserNumbers.splice(randomBall, 1);
+            console.log(`\t\t\tBola N.º: ${ballDispenserNumbers[randomIndexBall]} Turnos: ${userTurns} linea: ${lineMatch ? "Si" : "No"}`);
+            ballDispenserNumbers.splice(randomIndexBall, 1);
         }
-        for (let x = 0; x < cardBoard.length; x++) {
-            for (let z = 0; z < cardBoard[x].Value.length; z++) {
-                if (cardBoard[x].Value[z] == matchNumber) cardBoard[x].Value[z] = "X";
-            }            
+        if (lineMatch === false) {
+            for (let x = 0; x < lines.length; x++) {
+                if (lines[x].length == 5) {
+                    lineMatch = true;
+                    alert("Has conseguido completar una LINEA! \nSe restarán 7 puntos de tu contador de turnos en el proximo turno!");
+                }                
+            }1
         }
+        if (bingoMatch === false) {
+            if (lines[0].length == 5 && lines[1].length == 5 && lines[1].length == 5) {
+                lineMatch = true;
+                alert(`BINGO, Hasta ganado con un total de ${userTurns}`);
+                bingoMatch = true;
+                userPlaying = false
+            }
+        }
+
     }
-    
-    generateBallDispenser();
     wellcome();
 }
 
