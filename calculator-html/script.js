839ab0a1c60e7d44a1cd35739d2a8f5e890c8d2a
Jorge <jorgengarriga@gmail.com>
2020-09-17 17:54:21 +0200

    update

--- calculator-html/script.js
+++ calculator-html/script.js
@@ -4,24 +4,13 @@ const operandButton = document.querySelectorAll("[data-operand]");
 const clearButton = document.querySelector("[data-all-clear]");
 const eraseButton = document.querySelector("[data-erase-current-value]")
 const resultButton = document.querySelector("[data-result]")
+let currentValueDisplay = document.querySelector("[data-current-value-display]")
+let previousValueDisplay = document.querySelector("data-previous-value-display")
 
-
-
-let display
-
-numbersButton.forEach(numbers => {
-numbers.addEventListener("click", function () {
-    display = document.getElementById("test2").innerText = displaappendNumber(numbers.textContent)
+numbersButton.forEach(number => {
+number.addEventListener("click", function (x) {
+    currentDisplay.innerText += x.target.textContent
     })
 });
 
-operandButton.forEach(numbers => {
-    numbers.addEventListener("click", function () {
-        const x = document.getElementById("test2").innerText = numbers.innerText;
-    })
-});
-
-
-console.log(eraseButton.textContent);
-
-
+// 
